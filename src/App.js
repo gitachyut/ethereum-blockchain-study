69a169b70f32c62bb4561b9d42cf35cf26c63d76
gitachyut <linkinakd72@gmail.com>
2018-02-21 10:33:30 +0530

    new component added

--- src/App.js
+++ src/App.js
@@ -1,57 +1,14 @@
 import React, { Component } from 'react';
-// import Web3 from 'web3'
-import './App.css';
+import BlockChainData from './BlockChainData'
+
 
 class App extends Component {
-  state = {
-    socketUrl: 'wss://ws.blockchain.info/inv',
-    transitionType: {
-      "op":"unconfirmed_sub"
-    },
-    blocks: []
-  }
-  componentDidMount = () => {
-    // ping bloackchain 
-    this.blockChainData()
-  }
-
-  connectBlockChainScoket = () => {
-    return new WebSocket(this.state.socketUrl)
-  }
-  
-  blockChainData = () => {
-    const socketConnection = this.connectBlockChainScoket()
-    socketConnection.onopen = this.socketConnectionOnOpen(socketConnection)
-    socketConnection.onmessage = this.socketConnectionOnMsg
-  }
-
-
-  socketConnectionOnOpen =  ( connection ) =>  () => {
-    connection.send(JSON.stringify(this.state.transitionType))
-  }
-
-  socketConnectionOnMsg = (msg) => {
-    this.setState({
-      ...this.state,
-      blocks: [ ...this.state.blocks, JSON.parse(msg.data) ]
-    })    
-  }
-
   render() {
     return (
-      <div className="App">
-        <div className="list">
-          { this.state.blocks.map((block,index) => <BlockComponent key={block.x.hash} block={block} /> ) }  
-        </div>
-      </div>
+      <BlockChainData />
     );
   }
 }
 
-
-
-const BlockComponent = ( { block: { x: { hash }} } ) => <div className="item">
-  <div className="hash">Hash: {hash}</div>
-</div>
 
 export default App;
